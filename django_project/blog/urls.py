28c7eeb99db1da030af43cc2533794d037e69352
profake <nasifistiak@gmail.com>
2020-12-08 17:55:33 +0600

    footer booter etc added

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -3,7 +3,7 @@ from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,
 from . import views
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='blog-home'),
+    path('problems/', PostListView.as_view(), name='blog-home'),
     path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
