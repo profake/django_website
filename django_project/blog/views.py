1d2774ee84285dd95e25b73054855f5626c15826
profake <nasifistiak@gmail.com>
2020-12-29 21:47:45 +0600

    Added flexbox

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -12,6 +12,7 @@ from .models import Post
 def home(request):
     context = {
         'posts': Post.objects.all(),
+        'users': User.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
